4e8ad91d0a7ea19de5a6e1093049d87c76ea2fb8
denizderK <denizderya8668@gmail.com>
2023-08-30 22:50:13 +0300

    edt

---
+++ app/navbar/page.jsx
@@ -0,0 +1,9 @@
+"use client"
+//client side yapmak için bu direktifi kullanıyoruz bu broserde calışsın demek
+import React from 'react'
+
+export const  Navbar= () => {
+  return (
+    <div>Navbar</div>
+  )
+}
