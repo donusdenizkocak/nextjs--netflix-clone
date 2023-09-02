0cc1e6bfa7b433ad3565cc0ebfe4addde5e0f923
denizderK <denizderya8668@gmail.com>
2023-09-02 22:33:13 +0300

    edt

--- app/(private)/profile/page.jsx
+++ app/(private)/profile/page.jsx
@@ -4,7 +4,11 @@ export const metadata = {
   title: "Profile",
 };
 const Profile = () => {
-  return <div>Profile</div>;
+  return <div className="flex items-center justify-center mt-20">
+    <div className="flex flex-col">
+      <h1>Who &#39;s are you</h1>
+    </div>
+  </div>;
 };
 
 export default Profile;
